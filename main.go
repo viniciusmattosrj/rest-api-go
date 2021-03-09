e140893e285eb073a1a303b4455c5f6d4ea8da98
Vinicius Mattos <vinicius.mattos@zoop.com.br>
2021-03-08 22:53:52 -0300

    08032021 - Inclusão de source projeto.

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
+
+var people []Person
+
+func GetPeople(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(people)
+}
+
+func GetPerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Person{})
+}
+
+func CreatePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var person Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
+}
+
+func DeletePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+		json.NewEncoder(w).Encode(people)
+	}
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	s3Bucket := os.Getenv("S3_BUCKET")
+	secretKey := os.Getenv("SECRET_KEY")
+	fmt.Println(s3Bucket, secretKey)
+
+	router := mux.NewRouter()
+
+	people = append(people, Person{ID: "1", Firstname: "Luan", Lastname: "Vianna", Address: &Address{City: "Rio de Janeiro", State: "RJ"}})
+	people = append(people, Person{ID: "2", Firstname: "Camila", Lastname: "Moraes", Address: &Address{City: "Florianopolis", State: "SC"}})
+	people = append(people, Person{ID: "3", Firstname: "Leticia", Lastname: "Cardoso"})
+
+	router.HandleFunc("/contato", GetPeople).Methods("GET")
+	router.HandleFunc("/contato/{id}", GetPerson).Methods("GET")
+	router.HandleFunc("/contato/{id}", CreatePerson).Methods("POST")
+	router.HandleFunc("/contato/{id}", DeletePerson).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
